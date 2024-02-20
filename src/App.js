37a79ff4a0484aa23c3f7795d08f4541487baf12
dalimkumar604 <dalimkumar212582@gmail.com>
2024-02-20 13:22:10 +0600

    4Items

--- src/App.js
+++ src/App.js
@@ -23,74 +23,81 @@ function App() {
     titleSpark: <Spark color="#000" />,
 
     data: [
-      <TopLeft
-        title="Clever Idea"
-        titleColor="#000"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        infoColor="#000"
-        borderColor="#000"
-        background="#DFDE65"
-        shadow="#000"
-      />,
-      <TopMiddle
-        title="Simple Idea"
-        titleColor="#000"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        infoColor="#000"
-        borderColor="#000"
-        background="#FFDF88"
-        shadow="#000"
-      />,
-      <TopRight
-        title="Unique Idea"
-        titleColor="#000"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        infoColor="#000"
-        borderColor="#000"
-        background="#E7F3FF"
-        shadow="#000"
-      />,
-      <MiddleLeft
-        title="Modern Idea"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        borderColor="#000"
-        background="#EBBCAB"
-        shadow="#000"
-      />,
-      <MiddleRight
-        title="Smart Idea"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        borderColor="#000"
-        background="#FFD0E1"
-        shadow="#000"
-      />,
-      <BottomLeft
-        title="Creative Idea"
-        titleColor="#000"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        infoColor="#000"
-        borderColor="#000"
-        background="#FFA2B0"
-        shadow="#000"
-      />,
-      <BottomMiddle
-        title="Original Idea"
-        titleColor="#000"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        infoColor="#000"
-        borderColor="#000"
-        background="#E9DDFF"
-        shadow="#000"
-      />,
-      <BottomRight
-        title="Fresh Idea"
-        titleColor="#000"
-        info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
-        infoColor="#000"
-        borderColor="#000"
-        background="#E4E5E6"
-        shadow="#000"
-      />,
+      {
+        title: "Clever Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#DFDE65",
+        shadow: "#000",
+      },
+
+      {
+        title: "Simple Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#FFDF88",
+        shadow: "#000",
+      },
+      {
+        title: "Unique Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#E7F3FF",
+        shadow: "#000",
+      },
+      {
+        title: "Modern Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#EBBCAB",
+        shadow: "#000",
+      },
+      {
+        title: "Smart Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#FFD0E1",
+        shadow: "#000",
+      },
+      {
+        title: "Creative Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#FFA2B0",
+        shadow: "#000",
+      },
+
+      {
+        title: "Original Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#E9DDFF",
+        shadow: "#000",
+      },
+
+      {
+        title: "Fresh Idea",
+        titleColor: "#000",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+        infoColor: "#000",
+        borderColor: "#000",
+        background: "#E4E5E6",
+        shadow: "#000",
+      },
     ],
   });
 
--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,6 +1,13 @@
 import React from "react";
-
 import classes from "./MainComponent.module.css";
+import TopLeft from "../TopLeft";
+import TopMiddle from "../TopMiddle";
+import TopRight from "../TopRight";
+import MiddleLeft from "../MiddleLeft";
+import MiddleRight from "../MiddleRight";
+import BottomLeft from "../BottomLeft";
+import BottomMiddle from "../BottomMiddle";
+import BottomRight from "../BottomRight";
 const MainComponent = ({
   titleFontFamily,
   data,
@@ -9,6 +16,7 @@ const MainComponent = ({
   titleSpark,
   titleColor,
 }) => {
+  const svgs = [<TopLeft />, <TopMiddle />, <TopRight />, <MiddleLeft />];
   return (
     <div
       className="min-h-screen flex items-center justify-center w-full"
@@ -18,23 +26,64 @@ const MainComponent = ({
         className={[
           classes.wrapper,
           data.length > 6 && classes.moreThanSixItems,
+          data.length === 4 && classes.only4Items,
         ].join(" ")}
       >
         {" "}
         <div className={[classes.row1, classes.row].join(" ")}>
-          {" "}
-          {data.slice(0, 3).map((el, i) => (
+          <div
+            className={[classes.item, classes[`item-1`]].join(" ")}
+            style={{
+              "--titleFontFamily": titleFontFamily,
+              "--titleColor": titleColor,
+            }}
+          >
+            <TopLeft
+              title={data[0].title}
+              titleColor={data[0].titleColor}
+              info={data[0].info}
+              infoColor={data[0].infoColor}
+              borderColor={data[0].borderColor}
+              background={data[0].background}
+              shadow={data[0].shadow}
+            />
+          </div>
+          {data.length > 4 && (
             <div
-              className={[classes.item, classes[`item-${i + 1}`]].join(" ")}
-              key={i}
+              className={[classes.item, classes[`item-2`]].join(" ")}
               style={{
                 "--titleFontFamily": titleFontFamily,
-                "--titleColor": el.titleColor,
+                "--titleColor": titleColor,
               }}
             >
-              {el}
+              <TopMiddle
+                title={data[1].title}
+                titleColor={data[1].titleColor}
+                info={data[1].info}
+                infoColor={data[1].infoColor}
+                borderColor={data[1].borderColor}
+                background={data[1].background}
+                shadow={data[1].shadow}
+              />
             </div>
-          ))}
+          )}{" "}
+          <div
+            className={[classes.item, classes[`item-3`]].join(" ")}
+            style={{
+              "--titleFontFamily": titleFontFamily,
+              "--titleColor": titleColor,
+            }}
+          >
+            <TopRight
+              title={data[data.length > 4 ? 2 : 1].title}
+              titleColor={data[data.length > 4 ? 2 : 1].titleColor}
+              info={data[data.length > 4 ? 2 : 1].info}
+              infoColor={data[data.length > 4 ? 2 : 1].infoColor}
+              borderColor={data[data.length > 4 ? 2 : 1].borderColor}
+              background={data[data.length > 4 ? 2 : 1].background}
+              shadow={data[data.length > 4 ? 2 : 1].shadow}
+            />
+          </div>
         </div>{" "}
         <div className={[classes.row2, classes.row].join(" ")}>
           <div>
@@ -43,7 +92,15 @@ const MainComponent = ({
                 className={[classes.item, classes.middleLeft].join(" ")}
                 style={{ "--titleFontFamily": titleFontFamily }}
               >
-                {data[3]}
+                <MiddleLeft
+                  title={data[3].title}
+                  titleColor={data[3].titleColor}
+                  info={data[1].info}
+                  infoColor={data[3].infoColor}
+                  borderColor={data[3].borderColor}
+                  background={data[3].background}
+                  shadow={data[3].shadow}
+                />
               </div>
             )}
           </div>
@@ -71,26 +128,119 @@ const MainComponent = ({
                 className={[classes.item, classes.middleRight].join(" ")}
                 style={{ "--titleFontFamily": titleFontFamily }}
               >
-                {data[4]}
+                <MiddleRight
+                  title={data[4].title}
+                  titleColor={data[4].titleColor}
+                  info={data[4].info}
+                  infoColor={data[4].infoColor}
+                  borderColor={data[4].borderColor}
+                  background={data[4].background}
+                  shadow={data[4].shadow}
+                />
               </div>
             )}
           </div>
         </div>
         <div className={[classes.row3, classes.row].join(" ")}>
           {" "}
-          {data.slice(-3).map((el, i) => (
+          <div
+            className={[
+              classes.item,
+              classes[`item-6`],
+              classes.lastThree,
+            ].join(" ")}
+            style={{
+              "--titleFontFamily": titleFontFamily,
+              "--titleColor": titleColor,
+            }}
+          >
+            <BottomLeft
+              title={data[data.length > 6 ? 5 : data.length > 4 ? 3 : 2].title}
+              titleColor={
+                data[data.length > 6 ? 5 : data.length > 4 ? 3 : 2].titleColor
+              }
+              info={data[data.length > 6 ? 4 : data.length > 4 ? 3 : 2].info}
+              infoColor={
+                data[data.length > 6 ? 5 : data.length > 4 ? 3 : 2].infoColor
+              }
+              borderColor={
+                data[data.length > 6 ? 5 : data.length > 4 ? 3 : 2].borderColor
+              }
+              background={
+                data[data.length > 6 ? 5 : data.length > 4 ? 3 : 2].background
+              }
+              shadow={
+                data[data.length > 6 ? 5 : data.length > 4 ? 3 : 2].shadow
+              }
+            />
+          </div>
+          {data.length > 4 && (
             <div
               className={[
                 classes.item,
-                classes[`item-${i + 1}`],
+                classes[`item-7`],
                 classes.lastThree,
               ].join(" ")}
-              key={i}
-              style={{ "--titleFontFamily": titleFontFamily }}
+              style={{
+                "--titleFontFamily": titleFontFamily,
+                "--titleColor": titleColor,
+              }}
             >
-              {el}
+              <BottomMiddle
+                title={
+                  data[data.length > 6 ? 6 : data.length > 4 ? 4 : 1].title
+                }
+                titleColor={
+                  data[data.length > 6 ? 6 : data.length > 4 ? 4 : 1].titleColor
+                }
+                info={data[data.length > 6 ? 6 : data.length > 4 ? 4 : 1].info}
+                infoColor={
+                  data[data.length > 6 ? 6 : data.length > 4 ? 4 : 1].infoColor
+                }
+                borderColor={
+                  data[data.length > 6 ? 6 : data.length > 4 ? 4 : 1]
+                    .borderColor
+                }
+                background={
+                  data[data.length > 6 ? 6 : data.length > 4 ? 4 : 1].background
+                }
+                shadow={
+                  data[data.length > 6 ? 6 : data.length > 4 ? 4 : 1].shadow
+                }
+              />
             </div>
-          ))}
+          )}{" "}
+          <div
+            className={[
+              classes.item,
+              classes[`item-8`],
+              classes.lastThree,
+            ].join(" ")}
+            style={{
+              "--titleFontFamily": titleFontFamily,
+              "--titleColor": titleColor,
+            }}
+          >
+            <BottomRight
+              title={data[data.length > 6 ? 7 : data.length > 4 ? 5 : 3].title}
+              titleColor={
+                data[data.length > 6 ? 7 : data.length > 4 ? 5 : 3].titleColor
+              }
+              info={data[data.length > 6 ? 7 : data.length > 4 ? 5 : 3].info}
+              infoColor={
+                data[data.length > 6 ? 7 : data.length > 4 ? 5 : 3].infoColor
+              }
+              borderColor={
+                data[data.length > 6 ? 7 : data.length > 4 ? 5 : 3].borderColor
+              }
+              background={
+                data[data.length > 6 ? 7 : data.length > 4 ? 5 : 3].background
+              }
+              shadow={
+                data[data.length > 6 ? 7 : data.length > 4 ? 5 : 3].shadow
+              }
+            />
+          </div>
         </div>
       </div>
     </div>
